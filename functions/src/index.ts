5ee5b0986d46e210a776c1d84370c562cf58a13e
wdkkou <koukouki7547@gmail.com>
2020-04-25 22:27:46 +0900

    publishersをjsonデータで表示する

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,3 +6,19 @@ import * as functions from "firebase-functions";
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+import admin from "firebase-admin";
+import { collectionName } from "./services/mangarel/constants";
+
+admin.initializeApp();
+
+export const publishers = functions
+  .region("asia-northeast1")
+  .https.onRequest(async (req, res) => {
+    const snap = await admin
+      .firestore()
+      .collection(collectionName.publishers)
+      .get();
+    const data = snap.docs.map((doc) => doc.data());
+    res.send({ data });
+  });
